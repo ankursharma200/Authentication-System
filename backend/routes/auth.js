e3bb9afed3a5a2a2703cef2c38b7168c065046ab
Ankur Sharma <ankusharmapan125@gmail.com>
2026-02-17 22:08:08 +0530

    add login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+
+// Register 
 
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
@@ -19,4 +23,26 @@ router.post('/register', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+
+// Login
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'Invalid Credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid Credentials' });
+
+    const payload = { userId: user.id, name: user.name };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
