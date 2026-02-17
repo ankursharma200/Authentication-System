41705a8be8185cbed2164facf8b44667a890b1ae
Ankur Sharma <ankusharmapan125@gmail.com>
2026-02-17 21:53:42 +0530

    user model and registration route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', require('./routes/auth'));
+
 
 app.get("/", (req, res) => res.send("Backend is running!"));
 
